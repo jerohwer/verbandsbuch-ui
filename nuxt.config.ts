9c7260c0a695d9647a55cf70aba177bc88be71b8
Jeremy-Jerome <Jeremy-Jerome.Rohwer@edeka.de>
2026-01-22 10:49:30 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+    build: {
+        transpile: ['vuetify'],
+    },
+    vite: {
+        plugins: [
+            // @ts-expect-error
+            vuetify({ autoImport: true }),
+        ],
+        vue: {
+            template: {
+                transformAssetUrls,
+            },
+        },
+    },
+})
---
+++ pages/info.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+  <v-list lines="one">
+    <v-list-item
+        v-for="n in 3"
+        :key="n"
+        :title="'Item ' + n"
+        subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    ></v-list-item>
+  </v-list>
+    hfsdjhdsajflsdkfdsäafjkdjfkölsadjfkldöasjködfljlkj
+  </div>
+</template>
