270588cca50319586cf45a99f04c61123c1557d9
Jeremy <JeremyJeromeRohwer@gmx.de>
2026-01-29 08:33:27 +0100

    feat: Hinzufuegen BBZ-Logo als Facicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,14 @@
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
     build: {
         transpile: ['vuetify', '@pinia/nuxt'],
     },
     vite: {
         plugins: [
-            // @ts-expect-error
-            vuetify({ autoImport: true }),
+            vuetify({autoImport: true}),
         ],
         vue: {
             template: {
